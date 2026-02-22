78cb1b023aeef41a3f0e9edc836a58c21915655c
sommi991 <odumegwusomto9@gmail.com>
2026-02-22 23:20:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@core': path.resolve(__dirname, './src/core'),
-      '@niches': path.resolve(__dirname, './src/niches'),
-      '@shared': path.resolve(__dirname, './src/shared'),
-      '@selector': path.resolve(__dirname, './src/niche-selector'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-  },
-  server: {
-    port: 3000,
-  },
+  plugins: [react()]
 })
